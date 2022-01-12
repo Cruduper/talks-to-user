01aefbcd7e6767df1d03aacba5454ec9e3f7d670
Eric Crudup <briefcasemanx@gmail.com>
2022-01-12 00:03:28 -0800

    added 3 buttons that change color modes and display color mode name

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
+  $("button#blue-mode").click( function() {
+    $("body").removeClass()
+    $("body").toggleClass("blue");
+    $("ul").prepend("<li>You've entered Blue Mode</li>")
+    $("ul").children("li").first().click( function() {
+      (this).remove();
+    })
+  });
 
+
+  $("button#dark-mode").click( function() {
+    $("body").removeClass()
+    $("body").toggleClass("dark");
+    $("ul").prepend("<li>You've entered Dark Mode</li>")
+    $("ul").children("li").first().click( function() {
+      (this).remove();
+    })
+  });
+
+
+  $("button#red-mode").click( function() {
+    $("body").removeClass()
+    $("body").toggleClass("red");
+    $("ul").prepend("<li>You've entered Red Mode</li>")
+    $("ul").children("li").first().click( function() {
+      (this).remove();
+    })
+  });
 
 
 });
